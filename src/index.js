e5066b717750f2fff016645961dd8cc1375e17e1
bsegvic97 <62608075+bsegvic97@users.noreply.github.com>
2020-06-01 12:49:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,13 @@ document.getElementById("app").innerHTML = `
 var br = 0;
 var e = ["ana", "milovana", "banana", "ivana"];
 for (var i = 0; i < e.length; i++) {
+  //idi po svim rijecima
   for (var j = 0; j < e[i].length - 2; j++) {
+    //gledaj svaka 3 slova u nizu u pojedinoj rijeci
     var t = e[i][j] + e[i][j + 1] + e[i][j + 2];
-
+    //usporedi s "ana"
     if (t.toLowerCase() === "ana") br++;
   }
 }
 console.log(br);
-//Opet koristim imperativnu jer mi je tako najlakse pisat (dosad u zivotu vise manje uvijek imperativna, iako to tada nisam znao), a smatram da si od ova 4 "zadacica" mogu priustit da 2 budu u imperativnoj, iako smatram da su rjesenja imperativnom "bezvezna" i "vec videna"
+//ovdje sam koristio imperativnu jer funkcionalna vise nije toliko jednostavna za napisati
